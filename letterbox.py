dd14edf459014bf78a7000145b3fc956e9df9b62
Robert Forsman <git@thoth.purplefrog.com>
2014-09-14 13:27:34 -0400

    Adjust the bl_info to match the instructions at http://wiki.blender.org/index.php/Dev:2.5/Py/Scripts/Guidelines/Addons

--- letterbox.py
+++ letterbox.py
@@ -1,11 +1,13 @@
 bl_info = {
     "name" : "VSE letterboxing",
-    "category": "Sequencer",
+    "description": "A menu for adding letterboxes to VSE strips that don't have the exact same aspect ratio as the scene",
     "author" : "Robert Forsman <blender@thoth.purplefrog.com>",
     "version": (0,1),
-    "blender": (2,7,2),
+    "blender": (2,71,0),
     "location": "Video Sequence Editor > Strip",
-    "description": "A menu for adding letterboxes to VSE strips that don't have the exact same aspect ratio as the scene",
+    "warning": "",
+    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Sequencer/Letterbox",
+    "category": "Sequencer",
 }
 
 #
