51689214573859a6a1ae9a0653e5d8625e8e3498
Robert Forsman <git@thoth.purplefrog.com>
2015-05-22 12:34:05 -0400

    operators don't need bl_region_type

--- letterbox.py
+++ letterbox.py
@@ -224,7 +224,6 @@ class SequencerLetterbox(bpy.types.Operator):
     bl_idname = "sequencer.letterbox_center"
     bl_label = "Letterbox"
     bl_options = {'REGISTER', 'UNDO'}
-    bl_region_type='UI'
 
     align_x = bpy.props.FloatProperty(name="Align X", default=0.5, min=0, max=1.0,
                                       subtype='FACTOR', precision=4, step=100,
